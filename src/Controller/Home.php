4a15fe4d79cd38f6dde03896aa0a56c4ebac7638
elachraf48 <115742816+elachraf48@users.noreply.github.com>
2023-03-23 01:34:45 +0000

    up

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -39,9 +39,90 @@ class Home extends AbstractController {
      */
     public function add()
     {
-        return new Response(
-            'add post'
-        );
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('header.html.twig');
+    }
+    /**
+     * @Route("/home",name="post_home")
+     */
+    public function home()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('home.html.twig');
+    }
+    /**
+     * @Route("/about",name="post_about")
+     */
+    public function about()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('about.html.twig');
+    }
+    /**
+     * @Route("/resume",name="post_resume")
+     */
+    public function resume()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('resume.html.twig');
+    }
+    /**
+     * @Route("/services",name="post_services")
+     */
+    public function services()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('services.html.twig');
+    }
+    /**
+     * @Route("/skills",name="post_skills")
+     */
+    public function skills()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('skills.html.twig');
+    }
+    /**
+     * @Route("/projects",name="post_projects")
+     */
+    public function projects()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('projects.html.twig');
+    }
+    /**
+     * @Route("/blog",name="post_blog")
+     */
+    public function blog()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('blog.html.twig');
+    }
+    /**
+     * @Route("/contact",name="post_contact")
+     */
+    public function contact()
+    {
+        // return new Response(
+        //     'add post'
+        // );
+        return $this->render('contact.html.twig');
     }
 }
 
